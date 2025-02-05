3d49183acb1e76806ce63c8aca5434791cb96fcf
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-05 18:08:52 +0000

    Update Notes

---
+++ src/main/java/Notes.java
@@ -0,0 +1,54 @@
+//Make sure you do this before you use a scanner
+import java.util.Scanner;
+
+class Notes {
+	public static void main(String[] args) {
+
+		//Create the scanner
+		// Scanner in = new Scanner(System.in);
+
+		// // in.nextInt(); //Integer
+		// // in.nextDoiuble(); //Doubles
+		// // in.nextBoolean(); //Booleans
+		// // in.nextLine(); //Strings
+
+		// //input gets stored in a variable
+
+		// //prompt
+		// System.out.print("Input an integer: ");
+		// //input
+		// int num = in.nextInt();
+		// in.nextLine(); //clear the scanner
+		// //what we do with the input
+		// System.out.println(num);
+
+		// //prompt
+		// System.out.print("Input a number: ");
+		// //input
+		// double num2 = in.nextDouble();
+		// in.nextLine();
+		// //what we do with the input
+		// System.out.println(num2);
+
+		// //prompt
+		// System.out.print("Input a boolean: ");
+		// //input
+		// boolean flag = in.nextBoolean();
+		// in.nextLine();
+		// //what we do with the input
+		// System.out.println(flag);
+
+		// //prompt
+		// System.out.print("Input a word: ");
+		// //input
+		// String word = in.nextLine();
+		// //what we do with the input
+		// System.out.println(word);
+
+		System.out.println("Hello" + "World");
+		System.out.println("Hello" + 7);
+		System.out.println(5 + 5 + "Hello"); //10Hello
+		System.out.println("Hello" + 5 + 5);
+		System.out.println("Hello" + (5 + 5)); //Hello10
+	}
+}
